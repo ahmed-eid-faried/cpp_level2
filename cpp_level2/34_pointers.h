66661efd32ea09459b01abba5129d88e08e9fa3f
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-07-25 19:22:55 +0300

    derefrances pointer

--- cpp_level2/34_pointers.h
+++ cpp_level2/34_pointers.h
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
- 
+
 void Pointers() {
 	int x = 10;
 	int  y = 30;
@@ -21,6 +21,17 @@ void Pointers() {
 	cout << "VALUE OF pointer FOR Y ==>>  " << p << endl;
 	cout << "ADDRESS OF pointer FOR Y ==>>  " << &p << endl;
 	cout << "===============================" << endl;
+	cout << "VALUE Y by y ==>>  " << y << endl;
+	cout << "VALUE Y by *p ==>>  " << *p << endl;
+	cout << "=============== y=20 by y ================" << endl;
+	y = 20;
+	cout << "VALUE Y by y ==>>  " << y << endl;
+	cout << "VALUE Y by pointer ==>>  " << *p << endl;
+	cout << "=============== y=15 by *p ================" << endl;
+	*p = 15;
+	cout << "VALUE Y by y ==>>  " << y << endl;
+	cout << "VALUE Y by pointer ==>>  " << *p << endl;
+
 
 
 }
